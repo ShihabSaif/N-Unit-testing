84a23382f003417b0941715c78411f8fd5dae0f0
ShihabSaif <a.b.m.shihab57@gmail.com>
2018-05-29 22:25:06 +0600

    calculator testing

---
+++ CalculatorDemo/MyCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDemo
+{
+    public class MyCalculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double division(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ CalculatorDemo/MyCalculatorTest.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDemo
+{
+    [TestFixture]
+    class MyCalculatorTest
+    {
+        [TestCase(-0, -0)]
+        [TestCase(0, 0)]
+        [TestCase(11111111111111111111, 11111111111111111111)]
+        [TestCase(-12.11111111111111111111, -111111111111.11111111)]
+        [TestCase(-2.555555555, 2244)]
+        [TestCase(2244, 0)]
+        public void Add(double a, double b)
+        {
+            double x = a, y = b;
+            MyCalculator my = new MyCalculator();
+            Assert.AreEqual(x+y,my.Add(x,y));
+        }
+
+        [TestCase(-0, -0)]
+        [TestCase(0, 0)]
+        [TestCase(11111111111111111111, 11111111111111111111)]
+        [TestCase(-12.11111111111111111111, -111111111111.11111111)]
+        [TestCase(-2.555555555, 2244)]
+        [TestCase(2244, 0)]
+        public void Subtract(double a, double b)
+        {
+            double x = a, y = b;
+            MyCalculator my = new MyCalculator();
+            Assert.AreEqual(x - y, my.Subtract(x, y));
+        }
+
+        [TestCase(-0, -0)]
+        [TestCase(0, 0)]
+        [TestCase(11111111111111111111, 11111111111111111111)]
+        [TestCase(-12.11111111111111111111, -111111111111.11111111)]
+        [TestCase(-2.555555555, 2244)]
+        [TestCase(2244, 0)]
+        public void Multiply(double a, double b)
+        {
+            double x = a, y = b;
+            MyCalculator my = new MyCalculator();
+            Assert.AreEqual(x * y, my.multiply(x, y));
+        }
+
+        [TestCase(-0, -0)]
+        [TestCase(0, 0)]
+        [TestCase(11111111111111111111, 11111111111111111111)]
+        [TestCase(-12.11111111111111111111, -111111111111.11111111)]
+        [TestCase(-2.555555555, 2244)]
+        [TestCase(2244,0)]
+        public void Divide(double a, double b)
+        {
+            double x = a, y = b;
+            MyCalculator my = new MyCalculator();
+            Assert.AreEqual(x / y, my.division(x, y));
+        }
+
+
+    }
+}
